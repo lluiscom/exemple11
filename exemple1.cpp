920819a2f122264788b0377709dd295d6a02a7a0
lluiscom <158286702+lluiscom@users.noreply.github.com>
2024-06-26 18:57:05 +0200

    Primera actualitzacio: es crea el projecte

---
+++ exemple1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/* Per compilar s'ha de fer Ctrl+Shift+B (Run Build task), crea l'executable
+    Despres s'ha de executarintroduïnt el path a traves de la terminal del vsc i escribint el nom del .exe resultant apretant tab pel format correcte
+*/
+int main(){
+    cout <<"de executaa"<< endl;
+    string entraString;
+    cin >> entraString;
+    return 0;
+}
